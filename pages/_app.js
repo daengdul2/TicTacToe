4d09c61c01e80a5ec8f33e74901cd3aa4eeb9b8f
daengdul2 <106536045+daengdul2@users.noreply.github.com>
2025-09-25 04:02:42 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '../styles/globals.css';
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
